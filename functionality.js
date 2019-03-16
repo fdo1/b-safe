ee17e4b522053ba1afa53e3378854021c3be4d69
Fernando <fernando.madser@gmail.com>
2019-03-16 15:27:30 -0600

    Updates testimonials

--- functionality.js
+++ functionality.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
 
-	$('.include-header').html('<header id="top"><div class="nav-container"> <a href="#" class="hamburger-icon"><img src=""></a> <a href="index.html?name=fe&email=asd%40a&message=asd#top"><div class="logo"></div></a><ul class="menu-list"><li><a href="index.html?name=fe&email=asd%40a&message=asd#top">Inicio</a></li><li><a href="index.html?name=fe&email=asd%40a&message=asd#nuestros-servicios">Nuestros servicios</a></li><li><a href="index.html?name=fe&email=asd%40a&message=asd#contact">Contacto</a></li></ul>		<div class="nav-contact"><a href="https://api.whatsapp.com/send?phone=5215520673769"><div id="wa-icon"></div></a><h4>55 20 67 37 69</h4></div></div></header>');
+	$('.include-header').html('<header id="top"><div class="nav-container"> <a href="#" class="hamburger-icon"><img src=""></a> <a href="home.html"><div class="logo"></div></a><ul class="menu-list"><li><a href="home.html">Inicio</a></li><li><a href="home.html#nuestros-servicios">Nuestros servicios</a></li><li><a href="home.html#contact">Contacto</a></li></ul>		<div class="nav-contact"><a href="https://api.whatsapp.com/send?phone=5215540458063"><div id="wa-icon"></div></a><h4>55 40 45 80 63</h4></div></div></header>');
 
-	$('.include-footer').html('<h4>B-Safe | Broker de seguros y fianzas</h4><h4>!Síguenos!</h4><div class="social-box"><div><a href="#"><img src="images/facebook-icon.png" id="fb-icon"></a></div><div><a href="#"><img src="images/youtube-icon.png"></a></div><div><a href="#"><img src="images/twitter-icon.png"></a></div></div>');
+	$('.include-footer').html('<h4>B-Safe | Broker de seguros y fianzas</h4><h4>!Síguenos!</h4><div class="social-box"><div><a href="https://www.facebook.com/B-Safe-Broker-de-Seguros-y-Fianzas-134478256661299/"><img src="images/facebook-icon.png" id="fb-icon"></a></div><div><a href="https://twitter.com/BSafe_Seguros"><img src="images/twitter-icon.png"></a></div></div>');
 
 	$('.hamburger-icon').on('click', function(){ 
 				$('ul').toggleClass('show');
-				//$('ul').slideToggle(400);
 				console.log("click");
 	});
 
